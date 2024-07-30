0697d95663c63183b4980ff9b3b4ffae55b53afd
Abednego Setyawan <abednego.setyawan@gmail.com>
2024-07-30 14:27:37 +0700

    setup react app

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 const app = express()
 const port = 8080
 
-app.use(express.static("public"))
+app.use(bodyParser.json())
+app.use(express.static("dist"))
+
+app.post("/exec", (req, res) => {
+  res.send({ results: ["hello"] })
+})
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
