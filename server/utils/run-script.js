58cb2b62f14058efebd442182d67a2a805ca0caa
Abednego Setyawan <abednego.setyawan@gmail.com>
2024-08-12 09:27:17 +0700

    refactor run-script utils

--- server/utils/run-script.js
+++ server/utils/run-script.js
@@ -1,70 +1,25 @@
-const { parseSync } = require("@babel/core");
-const traverse = require("@babel/traverse").default;
-const t = require("@babel/types")
-
-const log = console.log
-const intervalIds = []
-const intervalDelays = []
-const timeoutIds = []
-const timeoutDelays = []
-
-function stopIntervals() {
-  for (let index = 0; index < intervalIds.length; index++) {
-    clearInterval(intervalIds[index])
-  }
-}
-
-function stopTimeouts() {
-  for (let index = 0; index < timeoutIds.length; index++) {
-    clearInterval(timeoutIds[index])
-  }
-}
-
-function runScript(script, callback) {
+const orig = console.log
+export function runScript(script, callback) {
   const logs = []
-  const ast = parseSync(script)
-
-  traverse(ast, {
-    CallExpression(path) {
-      const delay = path.node.arguments[1]
-      if (t.isIdentifier(path.node.callee, { name: "setInterval" })) {
-        intervalDelays.push(delay.value)
+  const logProxy = new Proxy(logs, {
+    set(target, property, value) {
+      target[property] = value
+      if (Array.isArray(value)) {
+        callback(target)
       }
-
-      if (t.isIdentifier(path.node.callee, { name: "setTimeout" })) {
-        timeoutDelays.push(delay.value)
-      }
+      return true
+    },
+    get(target, property) {
+      return target[property]
     }
   })
 
-  stopIntervals()
-  stopTimeouts()
-
   console.log = function () {
     const args = Array.from(arguments);
-    log.apply(console, args)
-    logs.push(args)
+    orig.apply(console, args)
+    logProxy.push(args)
   }
 
   new Function(script)()
-
-  for (let index = 0; index < intervalDelays.length; index++) {
-    intervalIds[index] = setInterval(() => {
-      callback(logs)
-    }, intervalDelays[index])
-  }
-
-  for (let index = 0; index < timeoutDelays.length; index++) {
-    timeoutIds[index] = setTimeout(() => {
-      callback(logs)
-    }, timeoutDelays[index])
-  }
-
   return logs
 }
-
-// log(runScript(`
-//   console.log("hello world")
-// `))
-
-module.exports = runScript
