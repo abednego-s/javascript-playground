453a1c6c279f18c13df20d271117c004ef77a8be
Abednego Setyawan <abednego.setyawan@gmail.com>
2024-08-13 14:24:38 +0700

    setup multi env webpack config

--- web/webpack.common.js
+++ web/webpack.common.js
@@ -11,7 +11,18 @@ module.exports = {
       template: path.join(__dirname, "public", "index.html"),
     }),
   ],
-  devServer: {
-    port: 8082,
-  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
+    ]
+  }
 };
---
+++ web/webpack.prod.js
@@ -0,0 +1,24 @@
+const path = require("path")
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "production",
+  output: {
+    filename: "bundle.[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+});
