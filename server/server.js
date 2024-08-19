aaf26ac432a9854dfd518da45d143b719892312f
Abednego Setyawan <abednego.setyawan@gmail.com>
2024-08-19 16:00:49 +0700

    remove websocket port

--- server/server.js
+++ server/server.js
@@ -2,13 +2,15 @@ const express = require("express")
 const WebSocket = require("ws")
 const bodyParser = require("body-parser")
 const path = require("path")
+const http = require("http")
 const runScript = require("./utils/run-script")
 const { createChannelName } = require("./utils/channels")
 require("dotenv").config();
 require("global-jsdom/register")
 
 const app = express()
-const wss = new WebSocket.Server({ port: 8081 })
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server })
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json())
@@ -66,6 +68,6 @@ wss.on("connection", (ws) => {
   });
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
 })
