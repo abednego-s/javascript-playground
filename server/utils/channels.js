21a33c6a37a8925d68a03e9d2711f84e0d0b178d
Abednego Setyawan <abednego.setyawan@gmail.com>
2024-08-21 12:14:34 +0700

    fix create channel name module

--- server/utils/channels.js
+++ server/utils/channels.js
@@ -1,13 +1,5 @@
 function createChannelName() {
-  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let result = "";
-
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * chars.length);
-    result += chars[randomIndex];
-  }
-
-  return result;
+  return Math.random().toString(36).substr(2, 9)
 }
 
 module.exports = { createChannelName }
