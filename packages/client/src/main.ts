7b532b9e0bbb2157af5ab026c31d26c4590628ac
Abednego Setyawan <abednego.setyawan@gmail.com>
2025-11-19 12:39:11 +0700

    install xterm

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -1,7 +1,9 @@
 import "./style.css";
+import "./xterm.css";
 import { EditorView, basicSetup } from "codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 import { okaidia } from "@uiw/codemirror-theme-okaidia";
+import { Terminal } from "@xterm/xterm";
 import { debounce } from "./utils/debounce";
 import { ResizablePanel } from "./utils/resizable-panel";
 
@@ -16,6 +18,8 @@ const outputElem = document.getElementById("output")!;
 const ws = connectToWs();
 createCodeEditor();
 createResizable();
+const term = createTerminal();
+term.write("Hello, World!");
 
 function createCodeEditor() {
   return new EditorView({
@@ -28,7 +32,6 @@ function createCodeEditor() {
       EditorView.updateListener.of((update) => {
         if (update.docChanged) {
           const value = update.state.doc.toString();
-          output.clear();
           runCode(value);
         }
       }),
@@ -40,11 +43,7 @@ function connectToWs() {
   const websocket = new WebSocket(wsUrl);
 
   websocket.onopen = function () {
-    console.log("[websocket]: ", wsUrl);
-    // setWs(websocket);
-    // setConnectedToWs(true);
-    // clearInterval(retryIntervalRef.current);
-    // retryIntervalRef.current = null;
+    console.log("[websocket] connected.");
   };
 
   websocket.onmessage = function (e) {
@@ -53,14 +52,11 @@ function connectToWs() {
       message: string;
     };
     console.log("[PARSED]: ", parsed);
-    output.write(parsed);
-    // proxiedLogs.push(parsed);
+    term.writeln(`${parsed.message}`);
   };
 
   websocket.onclose = function () {
     console.log("Connection closed!");
-    // setConnectedToWs(false);
-    // retryConnection();
   };
 
   return websocket;
@@ -70,47 +66,26 @@ function createResizable() {
   return new ResizablePanel("resizableContainer");
 }
 
-// function retryConnection() {
-//   setInterval(connectToWs, 5000)
-// }
+function createTerminal() {
+  const term = new Terminal({
+    convertEol: true,
+  });
+  term.open(outputElem);
+  return term;
+}
 
 const sendToWsServer = debounce((code) => {
   if (!code) return;
+  try {
+    new Function(code)();
+  } catch (error) {
+    term.writeln(`${error}`);
+    // output.write({ type: "error", message: `${error}` });
+    return false;
+  }
   ws.send(code);
 }, 1000);
 
 function runCode(code: string) {
-  try {
-    new Function(code)();
-  } catch (error) {
-    output.write({ type: "error", message: `${error}` });
-    return false;
-  }
   sendToWsServer(code);
 }
-const output = {
-  write(output: { type: "log" | "error"; message: string }) {
-    if (output.type === "error") {
-      const ul = document.createElement("ul");
-      const li = document.createElement("li");
-      li.classList.add(output.type);
-      li.innerText = output.message;
-      ul.appendChild(li);
-      outputElem.appendChild(ul);
-      return;
-    }
-
-    const ul =
-      outputElem.children.length === 0
-        ? document.createElement("ul")
-        : outputElem.getElementsByTagName("ul")[0];
-    const li = document.createElement("li");
-    li.classList.add(output.type);
-    li.innerText = output.message;
-    ul.appendChild(li);
-    outputElem.appendChild(ul);
-  },
-  clear() {
-    outputElem.replaceChildren();
-  },
-};
